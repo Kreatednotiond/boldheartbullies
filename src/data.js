0e2b8829d76ba5d6de76135308440b228bba7057
Kreatednotiond <kreatednotion.designs@gmail.com>
2025-12-18 11:36:58 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -61,6 +61,7 @@ export const SITE_DATA = {
       "plannedBreeding": {
         "stud": "CLB\u2019s Tyson (Outside Stud \u2014 owned by Cel's Legacy Bullies)",
         "timing": "Early\u2013Mid 2026",
+        "studHero": "./assets/tyson.jpg",
         "note": "Planned pairing. No stud page (outside stud)."
       }
     },
