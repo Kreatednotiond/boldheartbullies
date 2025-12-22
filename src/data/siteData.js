60bdbd2ae1f55bac0aaa1e398cecf990050c16af
Keon Jones <keonj.@Keons-MacBook-Pro.local>
2025-12-21 19:48:19 -0600

    Update site content

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -5,7 +5,7 @@ export const SITE_DATA = {
     location: "Wichita, Kansas",
     phone: "316-288-3306",
     email: "boldheartbullies@gmail.com",
-    instagram: "https://instagram.com/bold_heart_bullies",
+    instagram: "https://instagram.com/boldheart_bullies",
   },
 
   ownedStuds: [
